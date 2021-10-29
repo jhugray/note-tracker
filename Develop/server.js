76c37b250b90c66a341e7a611787614aba4d1093
Jess Hause Ugray <jess.ugray@gmail.com>
2021-10-29 09:38:49 -0400

    notes page linked

--- Develop/server.js
+++ Develop/server.js
@@ -1,42 +1,40 @@
-const express = require('express');
 const path = require('path')
+const express = require('express');
 const app = express();
 const PORT = 3001;
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
-app.use('/', (req, res) => {
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'))
-
 });
+// app.get('/notes/:note', (req, res) => {
+//   const chosen = req.params.note;
 
-app.get('/api/notes', (req, res) => {
-  return res.json(notes);
-});
+//   console.log(chosen);
 
-app.get('/api/notes/:note', (req, res) => {
-  const chosen = req.params.note;
+//   for (let i = 0; i < notes.length; i++) {
+//     if (chosen === notes[i].routeName) {
+//       return res.json(notes[i]);
+//     }
+//   }
 
-  console.log(chosen);
+//   return res.json(false);
+// });
 
-  for (let i = 0; i < notes.length; i++) {
-    if (chosen === notes[i].routeName) {
-      return res.json(notes[i]);
-    }
-  }
+// app.post('/notes', (req, res) => {
+//   const newNote = req.body;
 
-  return res.json(false);
-});
+//   console.log(newNote);
 
-app.post('/api/notes', (req, res) => {
-  const newNote = req.body;
+//   notes.push(newNote);
 
-  console.log(newNote);
-
-  notes.push(newNote);
-
-  res.json(newNote);
-});
+//   res.json(newNote);
+// });
 
 
 app.listen(PORT, () => {
