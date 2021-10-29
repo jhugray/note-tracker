ce8c3540e47e6d8850be14350e5705ebda3c93db
Jess Hause Ugray <jess.ugray@gmail.com>
2021-10-29 09:43:51 -0400

    api/notes path added

--- Develop/server.js
+++ Develop/server.js
@@ -9,11 +9,16 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
+app.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/db/db.json'))
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 // app.get('/notes/:note', (req, res) => {
 //   const chosen = req.params.note;
+
 
 //   console.log(chosen);
 
